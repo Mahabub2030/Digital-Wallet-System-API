d66b128cf13822045de33c7a90277d71e0e2f42e
Mahaub Alam <mahabubalom72091@gmail.com>
2025-08-01 13:34:53 +0300

    added some

--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -6,6 +6,9 @@ import { User } from "../user/user.model";
 import bcryptjs from "bcryptjs"
 import httpstatuscode from 'http-status-codes'
 
+import jwt, { JwtPayload } from 'jsonwebtoken';
+
+
 
 const credentialsLogin = async (payload:Partial<IUser>)=>{
     const {email, password} = payload;
@@ -15,15 +18,26 @@ const credentialsLogin = async (payload:Partial<IUser>)=>{
     if(!isUserExist){
         throw new AppError(httpstatuscode.CREATED,"Creadansthial login")
     }
-
-
-
     const isPasswordMatched = await bcryptjs.compare(password as string ,isUserExist.password as string)
     if(!isPasswordMatched){
         throw new Error("Password is Not Correct ")
     }
+
+    const jwtPayload={
+        userId:isUserExist._id,
+        email:isUserExist.email,
+        role:isUserExist.role
+    }
+
+
+    const accessToken = jwt.sign(jwtPayload,"secret",{
+        expiresIn:"1d"
+    })
+
+
+
     return {
-        email:isUserExist.email
+        accessToken
     }
 }
 
